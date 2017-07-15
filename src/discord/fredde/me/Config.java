ab3d9e193509020985353b8d216022479283130c
freddedotme <r5cx95tw232yvs8w@icloud.com>
2017-07-15 02:49:59 +0200

    Minor cleanup.

--- src/discord/fredde/me/Config.java
+++ src/discord/fredde/me/Config.java
@@ -8,37 +8,37 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public final class Config
+final class Config
 {
-	private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-	private static final String FILE_NAME = "Discord_config.json";
-	
-	private String botTokenId;
-	private long textChannelId;
-	
-	public void defaultSettings()
-	{
-		botTokenId = "";
-		textChannelId = -1;
-	}
-	
-	public static Config load() throws FileNotFoundException
-	{
-		return GSON.fromJson(new FileReader(FILE_NAME), Config.class);
-	}
-	
-	public void save() throws IOException
-	{
-		GSON.toJson(this, new FileWriter(FILE_NAME));
-	}
-	
-	public String getBotTokenID()
-	{
-		return botTokenId;
-	}
-	
-	public long getTextChannelID()
-	{
-		return textChannelId;
-	}
+  private static final Gson   GSON      = new GsonBuilder().setPrettyPrinting().create();
+  private static final String FILE_NAME = "plugins/Discord_config.json";
+
+  private String botTokenId;
+  private long   textChannelId;
+
+  void defaultSettings()
+  {
+    botTokenId = "";
+    textChannelId = -1;
+  }
+
+  static Config load() throws FileNotFoundException
+  {
+    return GSON.fromJson(new FileReader(FILE_NAME), Config.class);
+  }
+
+  void save() throws IOException
+  {
+    GSON.toJson(this, new FileWriter(FILE_NAME));
+  }
+
+  String getBotTokenID()
+  {
+    return botTokenId;
+  }
+
+  long getTextChannelID()
+  {
+    return textChannelId;
+  }
 }
