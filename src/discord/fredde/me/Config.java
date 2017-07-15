42595e9a79c7334cd1d89c6aa970ef012d7d21f5
freddedotme <r5cx95tw232yvs8w@icloud.com>
2017-07-15 02:38:23 +0200

    Minor cleanup

--- src/discord/fredde/me/Config.java
+++ src/discord/fredde/me/Config.java
@@ -10,34 +10,34 @@ import java.io.IOException;
 
 public final class Config
 {
-	private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-	
-	private String botTokenId;
-	private long textChannelId;
-	
-	public void defaultSettings()
-	{
-		botTokenId = "";
-		textChannelId = -1;
-	}
-	
-	public static Config load() throws FileNotFoundException
-	{
-		return GSON.fromJson(new FileReader("Discord_config.json"), Config.class);
-	}
-	
-	public void save(String fileName) throws IOException
-	{
-		GSON.toJson(this, new FileWriter(fileName));
-	}
-	
-	public String getBotTokenID()
-	{
-		return botTokenId;
-	}
-	
-	public long getTextChannelID()
-	{
-		return textChannelId;
-	}
+  private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+  private String botTokenId;
+  private long   textChannelId;
+
+  void defaultSettings()
+  {
+    botTokenId = "";
+    textChannelId = -1;
+  }
+
+  static Config load() throws FileNotFoundException
+  {
+    return GSON.fromJson(new FileReader("Discord_config.json"), Config.class);
+  }
+
+  public void save(String fileName) throws IOException
+  {
+    GSON.toJson(this, new FileWriter(fileName));
+  }
+
+  String getBotTokenID()
+  {
+    return botTokenId;
+  }
+
+  long getTextChannelID()
+  {
+    return textChannelId;
+  }
 }
--- src/discord/fredde/me/Main.java
+++ src/discord/fredde/me/Main.java
@@ -10,46 +10,42 @@ import java.io.FileNotFoundException;
 
 public class Main extends JavaPlugin implements Listener
 {
-	private Config config;
-	private DiscordBot bot;
-	
-	@Override
-	public void onEnable()
-	{
-		getServer().getPluginManager().registerEvents(this, this);
-	}
-	
-	@Override
-	public void onLoad()
-	{
-		try
-		{
-			config = Config.load();
-			bot = new DiscordBot(config, this);
-		}
-		catch(FileNotFoundException e)
-		{
-			config = new Config();
-			config.defaultSettings();
-			
-			getServer().getLogger().warning("[Discord] Created config file, please open it and set up the Discord bot.");
-		}
-	}
-	
-	@EventHandler
-	public void onAsyncPlayerChat(AsyncPlayerChatEvent e)
-	{
-		pushMessage(e.getPlayer().getName(), e.getMessage());
-	}
-	
-	private void pushMessage(String name, String message)
-	{
-		if(bot != null)
-			bot.sendFromMinecraft(name, message);
-	}
-	
-	public void sendFromDiscord(String name, String message)
-	{
-		getServer().broadcastMessage("<" + ChatColor.BLUE + "Discord" + ChatColor.RESET + name + "> " + message);
-	}
+  private DiscordBot bot;
+
+  @Override
+  public void onEnable()
+  {
+    getServer().getPluginManager().registerEvents(this, this);
+    getLogger().info("runs!");
+
+    Config config;
+    try
+    {
+      config = Config.load();
+      bot = new DiscordBot(config, this);
+    } catch (FileNotFoundException e)
+    {
+      config = new Config();
+      config.defaultSettings();
+
+      getServer().getLogger().warning("[Discord] Created config file, please open it and set up the Discord bot.");
+    }
+  }
+
+  @EventHandler
+  public void onAsyncPlayerChat(AsyncPlayerChatEvent e)
+  {
+    pushMessage(e.getPlayer().getName(), e.getMessage());
+  }
+
+  private void pushMessage(String name, String message)
+  {
+    if (bot != null)
+      bot.sendFromMinecraft(name, message);
+  }
+
+  void sendFromDiscord(String name, String message)
+  {
+    getServer().broadcastMessage("<" + ChatColor.BLUE + "Discord" + ChatColor.RESET + " " + name + "> " + message);
+  }
 }
